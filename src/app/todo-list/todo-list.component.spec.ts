7857f7f33ee888ab7567a1b53eefea712e55a957
Klas Mellbourn <klas@mellbourn.net>
2016-10-09 20:40:15 +0200

    testing: remove

--- src/app/todo-list/todo-list.component.spec.ts
+++ src/app/todo-list/todo-list.component.spec.ts
@@ -1,6 +1,7 @@
 /* tslint:disable:no-unused-variable */
 
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { By } from '@angular/platform-browser';
 import { TestBed, async } from '@angular/core/testing';
 import { TodoListComponent } from './todo-list.component';
 import { FormsModule } from '@angular/forms';
@@ -39,7 +40,6 @@ describe('Component: TodoList', () => {
   it('should list todo items', () => {
     // Arrange
     let fixture = TestBed.createComponent(TodoListComponent);
-    let app = fixture.debugElement.componentInstance;
 
     // Act
     fixture.detectChanges();
@@ -47,6 +47,23 @@ describe('Component: TodoList', () => {
     // Assert
     let compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('ol').children.length).toBe(todoList.length);
+  });
+
+  fdescribe('onRemove', () => {
+    it('should remove todo item', () => {
+      // Arrange
+      let fixture = TestBed.createComponent(TodoListComponent);
+      let expectedLength = todoList.length - 1;
+      fixture.detectChanges();
+      let compiled = fixture.debugElement.nativeElement;
+      let firstRemoveButton = fixture.debugElement.query(By.css('ol li button'));
+
+      // Act
+      firstRemoveButton.triggerEventHandler('click', null);
+
+      // Assert
+      expect(todoListServiceMock.removeItem).toHaveBeenCalledWith(todoList[0]);
+    });
   });
 
 });
