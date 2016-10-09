3725857ec58db9cc0e338e7e61833b200237d5a9
Klas Mellbourn <klas@mellbourn.net>
2016-10-09 14:19:57 +0200

    ng g component todo-item-editor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { TodoListService } from './todo-list.service';
+import { TodoItemEditorComponent } from './todo-item-editor/todo-item-editor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoListComponent
+    TodoListComponent,
+    TodoItemEditorComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/todo-item-editor/todo-item-editor.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { TodoItemEditorComponent } from './todo-item-editor.component';
+
+describe('Component: TodoItemEditor', () => {
+  it('should create an instance', () => {
+    let component = new TodoItemEditorComponent();
+    expect(component).toBeTruthy();
+  });
+});
