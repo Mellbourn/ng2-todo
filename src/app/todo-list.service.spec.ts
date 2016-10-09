70b5b51c4a20018b19a208a5928d52025b149eaa
Klas Mellbourn <klas@mellbourn.net>
2016-10-09 17:08:44 +0200

    testing todo-list.service

--- src/app/todo-list.service.spec.ts
+++ src/app/todo-list.service.spec.ts
@@ -2,6 +2,7 @@
 
 import { TestBed, async, inject } from '@angular/core/testing';
 import { TodoListService } from './todo-list.service';
+import { TodoItem } from './todo-item';
 
 describe('Service: TodoList', () => {
   beforeEach(() => {
@@ -13,4 +14,32 @@ describe('Service: TodoList', () => {
   it('should ...', inject([TodoListService], (service: TodoListService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('should remember todo list', inject([TodoListService], (service: TodoListService) => {
+    // Arrange
+    let newTodoItem = new TodoItem();
+    let list = service.getTodoList();
+    list.push(newTodoItem);
+
+    // Act
+    const modifiedList = service.getTodoList();
+
+    // Assert
+    expect(modifiedList).toContain(newTodoItem);
+  }));
+
+  it('should be able to remove items', inject([TodoListService], (service: TodoListService) => {
+    // Arrange
+    let newTodoItem = new TodoItem();
+    let list = service.getTodoList();
+    list.push(newTodoItem);
+
+    // Act
+    service.removeItem(newTodoItem);
+
+    // Assert
+    const modifiedList = service.getTodoList();
+    expect(modifiedList).not.toContain(newTodoItem);
+  }));
+
 });
