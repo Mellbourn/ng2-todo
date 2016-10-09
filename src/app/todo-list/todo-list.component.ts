556be1d2b96d07400b498cc1298e28a34847b23d
Klas Mellbourn <klas@mellbourn.net>
2016-10-09 14:13:35 +0200

    now getting todoitems and displaying them

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoListService } from '../todo-list.service';
+import { TodoItem } from '../todo-item';
 
 @Component({
   selector: 'app-todo-list',
@@ -6,10 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent implements OnInit {
+  todoItems: TodoItem[];
 
-  constructor() { }
+  constructor(private todoListService: TodoListService) { }
 
   ngOnInit() {
+    this.todoItems = this.todoListService.getTodoList();
   }
 
 }
