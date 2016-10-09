54fa4dd1b9f57a6e566447f6a1aad489720bbbea
Klas Mellbourn <klas@mellbourn.net>
2016-10-09 17:22:48 +0200

    testing: service todo-list.component

--- src/app/todo-list/todo-list.component.spec.ts
+++ src/app/todo-list/todo-list.component.spec.ts
@@ -1,11 +1,34 @@
 /* tslint:disable:no-unused-variable */
 
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { TestBed, async } from '@angular/core/testing';
 import { TodoListComponent } from './todo-list.component';
+import { FormsModule } from '@angular/forms';
+import { TodoListService } from '../todo-list.service';
 
 describe('Component: TodoList', () => {
+  let todoListServiceMock: TodoListService;
+
+  beforeEach(() => {
+    todoListServiceMock = jasmine.createSpyObj('TodoListService', ['getTodoList', 'removeItem'])
+    TestBed
+      .configureTestingModule({
+        imports: [
+          FormsModule
+        ],
+        declarations: [
+          TodoListComponent
+        ],
+        schemas: [CUSTOM_ELEMENTS_SCHEMA],
+        providers: [
+          { provide: TodoListService, useValue: todoListServiceMock }
+        ]
+      });
+  });
+
   it('should create an instance', () => {
-    let component = new TodoListComponent();
-    expect(component).toBeTruthy();
+    let fixture = TestBed.createComponent(TodoListComponent);
+    let app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
   });
 });
