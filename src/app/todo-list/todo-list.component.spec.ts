0bd0c10fdc8d0ed37e0138e340b81d04bcdbef4b
Klas Mellbourn <klas@mellbourn.net>
2016-10-09 20:20:24 +0200

    testing: added some more sensible tests of todo-list.component

--- src/app/todo-list/todo-list.component.spec.ts
+++ src/app/todo-list/todo-list.component.spec.ts
@@ -5,12 +5,16 @@ import { TestBed, async } from '@angular/core/testing';
 import { TodoListComponent } from './todo-list.component';
 import { FormsModule } from '@angular/forms';
 import { TodoListService } from '../todo-list.service';
+import { TodoItem } from '../todo-item';
 
 describe('Component: TodoList', () => {
   let todoListServiceMock: TodoListService;
+  let todoList: TodoItem[];
 
   beforeEach(() => {
     todoListServiceMock = jasmine.createSpyObj('TodoListService', ['getTodoList', 'removeItem'])
+    todoList = [{ text: 'foo', done: true }, { text: 'bar', done: false }];
+    (<jasmine.Spy>todoListServiceMock.getTodoList).and.returnValue(todoList);
     TestBed
       .configureTestingModule({
         imports: [
@@ -31,4 +35,18 @@ describe('Component: TodoList', () => {
     let app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
+
+  it('should list todo items', () => {
+    // Arrange
+    let fixture = TestBed.createComponent(TodoListComponent);
+    let app = fixture.debugElement.componentInstance;
+
+    // Act
+    fixture.detectChanges();
+
+    // Assert
+    let compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('ol').children.length).toBe(todoList.length);
+  });
+
 });
