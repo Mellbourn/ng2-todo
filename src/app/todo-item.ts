bc0f2b5bf51de3604709eb0115790813f7a521f2
Klas Mellbourn <klas@mellbourn.net>
2016-10-09 13:58:41 +0200

    implemented list service, added todo-item class

---
+++ src/app/todo-item.ts
@@ -0,0 +1,4 @@
+export class TodoItem {
+  text: string;
+  done: boolean;
+}
--- src/app/todo-list.service.ts
+++ src/app/todo-list.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
+import { TodoItem } from './todo-item';
 
 @Injectable()
 export class TodoListService {
 
-  constructor() { }
+  private readonly todoItems: TodoItem[];
+
+  getTodoList(): TodoItem[] {
+    return this.todoItems;
+  }
+
+  constructor() {
+    this.todoItems = [
+      { text: 'create app', done: false },
+      { text: 'upload to github', done: false },
+      { text: 'post app on social', done: false }
+    ];
+  }
 
 }
