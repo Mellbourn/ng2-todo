758431db824c059ed6fa624b927d996b33be182f
Klas Mellbourn <klas@mellbourn.net>
2016-10-09 14:51:03 +0200

    now pushing new items on list, but they are emptey?

--- src/app/todo-item-editor/todo-item-editor.component.ts
+++ src/app/todo-item-editor/todo-item-editor.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { TodoItem } from '../todo-item';
+import { TodoListService } from '../todo-list.service';
 
 @Component({
   selector: 'app-todo-item-editor',
@@ -10,16 +11,21 @@ import { TodoItem } from '../todo-item';
 export class TodoItemEditorComponent implements OnInit {
   todoItemForm: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
-
-  ngOnInit() {
+  constructor(
+    private fb: FormBuilder,
+    private todoList: TodoListService) {
+    // TODO: move back to ngOnInit
     this.todoItemForm = this.fb.group({
       'text': [],
       'done': [false]
     });
   }
 
+  ngOnInit() {
+  }
+
   onSubmit(todoItem: TodoItem): void {
     console.log(todoItem);
+    this.todoList.getTodoList().push({ text: todoItem.text, done: todoItem.done });
   }
 }
