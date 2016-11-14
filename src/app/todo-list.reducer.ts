36edfdb5a690a4556d89fad9aa1155a7b0928c58
Klas Mellbourn <klas@mellbourn.net>
2016-11-14 21:45:53 +0100

    refactored

--- src/app/todo-list.reducer.ts
+++ src/app/todo-list.reducer.ts
@@ -12,30 +12,39 @@ const initalTodoItems: Immutable.List<TodoItem> = Immutable.List.of(
 
 const initialState = Immutable.Map({ todoItems: initalTodoItems });
 
+function addTodoItem(state: Immutable.Map<string, any>, itemToAdd: TodoItem): Immutable.Map<string, any> {
+  const extendedTodoItems = state.get('todoItems').push(itemToAdd);
+  return state.set('todoItems', extendedTodoItems);
+}
+
+function removeTodoItem(state: Immutable.Map<string, any>, itemToRemove: TodoItem): Immutable.Map<string, any> {
+  const indexOfItemToRemove = state.get('todoItems').lastIndexOf(itemToRemove);
+  const splicedArray = state.get('todoItems').delete(indexOfItemToRemove);
+  return state.set('todoItems', splicedArray);
+}
+
+function toggleItemDone(state: Immutable.Map<string, any>, itemToToggle: TodoItem): Immutable.Map<string, any> {
+  const todoItems = state.get('todoItems');
+  const indexOfItemToToggle = todoItems.lastIndexOf(itemToToggle);
+  const toggledItem = { text: itemToToggle.text, done: !itemToToggle.done };
+  const newTodoItems = todoItems.set(indexOfItemToToggle, toggledItem);
+  return state.set('todoItems', newTodoItems);
+}
+
 export const todoListReducer: ActionReducer<Immutable.Map<string, any>> =
   (state = initialState, action: Action) => {
     switch (action.type) {
 
       case ADD_TODO_ITEM:
-        const itemToAdd: TodoItem = action.payload;
-        const extendedTodoItems = state.get('todoItems').push(itemToAdd);
-        return state.set('todoItems', extendedTodoItems);
+        return addTodoItem(state, action.payload);
 
       case REMOVE_TODO_ITEM:
-        const itemToRemove: TodoItem = action.payload;
-        const indexOfItemToRemove = state.get('todoItems').lastIndexOf(itemToRemove);
-        const splicedArray = state.get('todoItems').delete(indexOfItemToRemove);
-        return state.set('todoItems', splicedArray);
+        return removeTodoItem(state, action.payload);
 
       case TOGGLE_ITEM_DONE:
-        const itemToToggle: TodoItem = action.payload;
-        const todoItems = state.get('todoItems');
-        const indexOfItemToToggle = todoItems.lastIndexOf(itemToToggle);
-        const toggledItem = { text: itemToToggle.text, done: !itemToToggle.done };
-        const newTodoItems = todoItems.set(indexOfItemToToggle, toggledItem);
-        return state.set('todoItems', newTodoItems);
+        return toggleItemDone(state, action.payload);
 
-      default:;
+      default: ;
         return state;
     }
   };
