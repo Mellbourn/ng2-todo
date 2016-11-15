9fb8bed7b6824d951a7bce6aa2e11cce480b5b5a
Klas Mellbourn <klas@mellbourn.net>
2016-11-15 18:12:44 +0100

    better mapping, still a bit wierd though

--- src/app/todo-list.service.ts
+++ src/app/todo-list.service.ts
@@ -4,6 +4,8 @@ import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Rx';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/let';
+import '@ngrx/core/add/operator/select';
 import { List } from 'immutable';
 
 import { addTodoItem, removeTodoItem, toggleDone } from './todo-list.action-creators';
@@ -12,14 +14,18 @@ import { AppState } from './app-state';
 @Injectable()
 export class TodoListService {
 
-  private readonly todoItems: Observable<List<TodoItem>>;
+  private readonly state: Observable<AppState>;
+
+  getTodoItemsState(state$: Observable<any>) {
+    return state$.select(state => state.todoItems);
+  }
 
   getTodoList(): Observable<List<TodoItem>> {
-    return this.todoItems;
+    return this.state.map(s => s.get('todoItems'));
   }
 
   getItemsLeft(): Observable<number> {
-    return (<Observable<List<TodoItem>>>this.store.select('todoItems').do(i => console.log('i', i))).map(s => s.get(0).count());
+    return this.state.map(s => s.get('todoItems').filter(i => !i.done).count());
   }
 
   removeItem(itemToRemove: TodoItem) {
@@ -35,7 +41,6 @@ export class TodoListService {
   }
 
   constructor(private store: Store<AppState>) {
-    this.todoItems = <Observable<List<TodoItem>>>store.select('todoItems');
+    this.state = this.store.let(this.getTodoItemsState);
   }
-
 }
